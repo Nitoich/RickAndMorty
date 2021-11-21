c7f90f7b88b86a61686625f2e8d15128070b1545
Nitoich <bcmamka@gmail.com>
2021-11-21 15:57:48 +0300

    Не большие улучшения дизайна

--- js/mainfunction.js
+++ js/mainfunction.js
@@ -5,4 +5,18 @@ burger_btn.addEventListener('click', function(event) {
     this.classList.toggle('active');
     nav.classList.toggle('active');
     document.body.classList.toggle('overflow');
+    window.location.href = "#";
+});
+
+document.querySelectorAll('a.gradient-link').forEach(el => {
+    el.addEventListener('mousemove', function(event) {
+        xpos = event.offsetX;
+        ypos = event.offsetY;
+
+        this.style.backgroundImage = 'radial-gradient(at ' + xpos + 'px ' + ypos + 'px, rgba(9,250,0,1) 2px, rgba(0,212,255,1) 50%)';
+    });
+
+    el.addEventListener('mouseleave', function(event) {
+        this.style.backgroundImage = '';
+    });
 });
