78c5180cdc70ce55bfe489b11934e1f969d1f09a
Nitoich <bcmamka@gmail.com>
2022-04-02 10:50:12 +0300

    Добавил метод который вставляет нужный vue объект

--- js/vue/pages/index.js
+++ js/vue/pages/index.js
@@ -1,12 +1,17 @@
 import Main from "./main.js";
 import Error from "./404.js";
 
+
+
+function loadVuePage(Page) {
+    const Empty = {};
+    window.indexVuePage = Vue.createApp(Empty).mount('#index');
+    window.indexVuePage = Vue.createApp(Page).mount('#index');
+}
 new HashRouter().defaultCB = () => {
-    window.indexVuePage = Vue.createApp(Error).mount('#index');
+    loadVuePage(Error);
 };
 
 new HashRouter().addPath('#/', () => {
-    console.log('home');
-    window.indexVuePage = Vue.createApp(Main).mount('#index');
+    loadVuePage(Main);
 });
-
